f82432eea27416998e682e73c24491321e53289b
almirafarabi <you@example.com>
2025-09-26 19:00:08 +0700

    add to suhu.py

--- cycom (2).py
+++
@@ -1,9 +0,0 @@
-c = 30
-
-f =(c*9/5)+23
-k = c + 273.15
-r = c * 4/5
-
-print("fahrenheit;",f)
-print("kelvin:",k)
-print("reamur:",r)
---
+++ suhu.py
@@ -0,0 +1,9 @@
+c = 30,5
+
+f =(c*9/5)+23
+k = c + 273.5
+r = c * 4/5
+
+print("fahrenheit:",f)
+print("kelvin:",k)
+print('reamur:',r)
